52c8b07d3a565e3807ec9d64179f30e089c2fd04
yimiqisan <yimiqisan@gmail.com>
2011-11-04 15:14:50 +0800

    add handler

--- handler/commonHandler.py
+++ handler/commonHandler.py
@@ -8,6 +8,8 @@ Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 """
 
 from baseHandler import BaseHandler
+from apps.timeline import TimeLine
+from apps.tools import session
 
 class RootHandler(BaseHandler):
     def get(self):
@@ -15,12 +17,41 @@ class RootHandler(BaseHandler):
 
 class TestHandler(BaseHandler):
     def get(self):
-        self.render("google.html")
+        self.render("test.html")
 
 class FeedbackHandler(BaseHandler):
+    
     def get(self):
-        self.render("feedback.html")
+        tl = TimeLine()
+        r = tl._api.list()
+        if r[0]:
+            return self.render("feedback.html", **{'fb_list': r[1]})
+        else:
+            return self.render("feedback.html", **{'warning': r[1]})
+    
+    @session
+    def post(self):
+        uid = self.SESSION['uid']
+        c = self.get_argument('content', None)
+        n = self.get_secure_cookie("user") if uid else u'匿名驴友'
+        tl = TimeLine()
+        r = tl._api.save(owner=uid, column=u'weibo', subject=u'feedback', content=c, nick=n)
+        if r[0]:
+            return self.redirect("/feedback")
+        else:
+            return self.render("feedback.html", **{'warning': r[1]})
 
 class Error404Handler(BaseHandler):
     def get(self):
         self.render("404.html")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- handler/profileHandler.py
+++ handler/profileHandler.py
@@ -7,52 +7,65 @@ Created by 刘 智勇 on 2011-09-24.
 Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 """
 
-from base import BaseHandler
+from baseHandler import BaseHandler
 from apps.user import User
+from apps.tools import session
 
 class LoginHandler(BaseHandler):
     def get(self):
         self.render('login.html')
-        
+    
+    @session
     def post(self):
-        u = User()
         n = self.get_argument('nick', None)
         if n is None:return self.render('login.html', **{'warning': '请先报上名号'})
         p = self.get_argument('password', None)
         if p is None:return self.render('login.html', **{'warning': '您接头暗号是？'})
-        r = u._api.login(n, p)
+        u = User()
+        r = u.login(n, p)
         if r[0]:
             self.set_secure_cookie("user", n, 1)
+            self.SESSION['uid']=u._id
             self.redirect('/account/profile')
         else:
             return self.render('login.html', **{'warning': r[1]})
-    
+
 class RegisterHandler(BaseHandler):
     def get(self):
         self.render('register.html')
-    
+
+    @session
     def post(self):
-        u = User()
         n = self.get_argument('nick', None)
         if n is None:return self.render('register.html', **{'warning': '请先报上名号'})
         p = self.get_argument('password', None)
         if p is None:return self.render('register.html', **{'warning': '您接头暗号是？'})
-        r = u._api.register(n, p)
+        u = User()
+        r = u.register(n, p)
         if r[0]:
             self.set_secure_cookie("user", n, 1)
+            self.SESSION['uid']=u._id
             self.redirect('/account/profile')
         else:
             return self.render('register.html', **{'warning': r[1]})
     
 class LogoutHandler(BaseHandler):
+    @session
     def get(self):
         self.clear_cookie("user")
+        del self.SESSION['uid']
         self.redirect('/')
     
 class ProfileHandler(BaseHandler):
+    @session
     def get(self):
         self.render('profile.html')
     
 class SettingHandler(BaseHandler):
+    @session
     def get(self):
+        self.render('setting.html')
+    
+    @session
+    def post(self):
         self.render('setting.html')
